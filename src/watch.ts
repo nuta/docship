ade7066bccf5c5b43c49cddd5af1009bb67a7271
Seiya Nuta <nuta@seiya.me>
2024-09-14 20:40:03 +0900

    Use Vite as preview server

--- src/watch.ts
+++ src/watch.ts
@@ -1,10 +1,8 @@
-import fs from "node:fs/promises";
-import http from "node:http";
 import type { Config } from "./config.js";
-import { progress, srcDir } from "./utils.js";
+import { progress } from "./utils.js";
 import { build } from "./build.js";
-import { TempDir } from "./tmpdir.js";
-import path from "node:path";
+import { createServer } from 'vite'
+import fs from "fs/promises";
 
 export interface WatchOptions {
   inDir: string;
@@ -13,102 +11,42 @@ export interface WatchOptions {
   config?: Config;
 }
 
-const HOT_RELOAD_SCRIPT = `
-<script>
-  (async () => {
-    const resp = await fetch('http://localhost:9911/_reload');
-    if (resp.status === 201) {
-      window.location.reload();
-    }
-  })()
-</script>
-`;
-
 export async function watch(options: WatchOptions) {
   let reloadResolvers: any[] = [];
   const triggerBuild = async () => {
-    await build({
-      inDir: options.inDir,
-      outDir: options.outDir,
-      builderName: options.builderName,
-      config: options.config,
-    });
+    try {
 
-    for (const resolve of reloadResolvers) {
-      resolve();
-    }
-    reloadResolvers = [];
-  };
-
-  const server = http.createServer(async (req, res) => {
-    if (!req.url) {
-      res.writeHead(400);
-      res.end();
-      return;
-    }
-
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    if (url.pathname === "/_reload") {
-      const promise = new Promise<void>((resolve) => {
-        reloadResolvers.push(resolve);
+      await build({
+        inDir: options.inDir,
+        outDir: options.outDir,
+        builderName: options.builderName,
+        config: options.config,
       });
 
-      await promise;
-      res.writeHead(201);
-      res.end("TRIGGERED\n");
-      return;
-    }
-
-    console.log(`${req.method} ${url.pathname}`);
-
-    let localRelPath;
-    if (url.pathname === "/") {
-      localRelPath = "/index.html";
-    } else {
-      localRelPath = url.pathname;
-    }
-
-    let localPath = path.join(options.outDir, localRelPath);
-    try {
-      await fs.access(localPath + ".html", fs.constants.R_OK);
-      localPath += ".html";
-    } catch (e) {}
-
-    try {
-      if ((await fs.stat(localPath)).isDirectory()) {
-        localPath = path.join(localPath, "index.html");
+      for (const resolve of reloadResolvers) {
+        resolve();
       }
-    } catch (e) {}
-
-    try {
-      await fs.access(localPath, fs.constants.R_OK);
+      reloadResolvers = [];
     } catch (e) {
-      res.writeHead(404);
-      res.end();
-      return;
+      console.error(`failed to build: ${(e as any).stack}`);
     }
-
-    let body;
-    if (path.extname(localPath) === ".html") {
-      body = await fs.readFile(localPath, "utf-8");
-      res.setHeader("Content-Type", "text/html");
-      body = body.replace("</body>", HOT_RELOAD_SCRIPT + "</body>");
-    } else {
-      body = await fs.readFile(localPath);
-    }
-
-    res.writeHead(200);
-    res.end(body);
-  });
-  server.listen(9911, "127.0.0.1");
+  };
 
   await triggerBuild();
-  progress("Open http://localhost:9911 to preview your site");
-  progress("Watching for changes...");
-  Promise.all([
-    watchFiles(triggerBuild, options.inDir),
-    watchFiles(triggerBuild, srcDir()),
-  ]);
+  watchFiles(triggerBuild, options.inDir, options.outDir).catch(console.error);
+
+  const previewServer = await createServer({
+    configFile: false,
+    root: options.outDir,
+    server: {
+      port: 1337,
+    },
+  })
+  await previewServer.listen()
+
+  progress("started preview server");
+  previewServer.printUrls()
+  previewServer.bindCLIShortcuts({ print: true })
 }
 
 function shouldReload(filePath: string) {
@@ -120,17 +58,13 @@ function shouldReload(filePath: string) {
   );
 }
 
-async function watchFiles(triggerBuild: () => Promise<void>, baseDir: string) {
-  for await (const ev of fs.watch(baseDir, { recursive: true })) {
-    if (!ev.filename || !shouldReload(ev.filename)) {
+async function watchFiles(triggerBuild: () => Promise<void>, inDir: string, outDir: string) {
+  for await (const ev of fs.watch(inDir, { recursive: true })) {
+    if (!ev.filename || !shouldReload(ev.filename) || ev.filename.startsWith(outDir)) {
       continue;
     }
 
     progress(`File changed: ${ev.filename}`);
-    try {
-      await triggerBuild();
-    } catch (e) {
-      console.error(`failed to build: ${(e as any).stack}`);
-    }
+    await triggerBuild();
   }
 }
