8471296f11e2c326804fa48a9e790fde5b6bc986
Seiya Nuta <nuta@seiya.me>
2024-09-24 10:16:31 +0900

    Fix `--config` option

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import type { Config } from "./config.js";
 import { watch } from "./watch.js";
+import { progress } from "./utils.js";
 
 process.on("unhandledRejection", (ev) => {
   console.warn("unhandled rejection:", ev);
@@ -39,11 +40,12 @@ async function main() {
     })
     .parseSync();
 
-
+  const configPath = args.config ? path.resolve(args.config) : path.resolve(args.indir, "docship.mjs");
+  progress(`Loading config from ${configPath}`);
 
   let mod: { default: Config } | undefined;
   try {
-    mod = await import(args.config ?? path.resolve(args.indir, "docship.mjs"));
+    mod = await import(configPath);
   } catch (e) {
     if ((e as any).code === "ERR_MODULE_NOT_FOUND") {
       console.error("Missing docship.mjs, using defaults...");
